75f5297c5538378be9f5b54f7f690bb8bf29ee01
Jack <jack20220723@gmail.com>
2024-01-21 22:04:50 +0800

    Support creating pipelines and secrets

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="screwdriver-cd-python-sdk",
     version="0.0.1",
-    description="Screwdriver CD Python SDK",
+    description="Screwdriver CD Python Software Development Kit (SDK) used to write Python automation scripts that create and manage resources in Screwdriver",
     url="https://github.com/QubitPi/screwdriver-cd-python-sdk",
     author="Jiaqi liu",
     author_email="jack20220723@gmail.com",
