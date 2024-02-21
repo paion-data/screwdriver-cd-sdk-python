82ad076c6209f01e3d2869ff95632bde10e9a9f4
Jiaqi Liu <jack20220723@gmail.com>
2024-02-21 15:12:28 +0800

    Implement Screwdriver initializer (#4)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="screwdriver-cd-python-sdk",
-    version="0.0.9",
+    version="0.1.0",
     description="Screwdriver CD Python Software Development Kit (SDK) used to write Python automation scripts that create and manage resources in Screwdriver",
     url="https://github.com/QubitPi/screwdriver-cd-python-sdk",
     author="Jiaqi liu",
